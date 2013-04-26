a4c4fe049956efb6ccae472d9d3f237c517fe7e3
Francis Brosnan Blázquez <francis@aspl.es>
2013-04-26 10:40:56 +0000

    nopoll: * [fix] Closing 0.2.3 release.

---
+++ get-version.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+from core_admin_common import command
+import sys
+
+(status, info) = command.run ("LANG=C svn update .")
+if status:
+    print "ERROR: unable to get subversion version: %s" % info
+    sys.exit (-1)
+
+# get versision
+revision = info.split (" ")[2].replace (".", "").strip ()
+print "INFO: Revision found: %s" % revision
+
+version = open ("VERSION").read ().split (".b")[0].strip ()
+version = "%s.b%s" % (version, revision)
+print "INFO: Updated vesion to: %s" % version
+
+open ("VERSION", "w").write ("%s\n" % version)
+
+
