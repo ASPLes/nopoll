c52bc980e6bbbffaaa872af270b617af8f24fef0
Francis Brosnan Blázquez <francis@aspl.es>
2015-04-13 10:29:22 +0000

    nopoll: * [fix] Updating buildrpm.sh

--- buildrpm.sh
+++ buildrpm.sh
@@ -15,6 +15,8 @@ echo "Calling to compile packages.."
 LANG=C rpmbuild -ba --define '_topdir /usr/src/nopoll/trunk/rpm' rpm/SPECS/nopoll.spec
 
 echo "Output ready at rpm/RPMS"
-find rpm/RPMS -type f
+find rpm/RPMS -type f -name '*.rpm' > rpm/RPMS/files
+cat rpm/RPMS/files
+
 
 
