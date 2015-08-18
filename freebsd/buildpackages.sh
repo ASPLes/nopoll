443bb27cb01ae2be3b240c8db987253ce2639fff
Francis Brosnan Blázquez <francis@aspl.es>
2015-08-18 15:03:24 +0000

    nopoll: * [fix] Added support to provide FreeBSD packages (for now 9.X releases).

---
+++ freebsd/buildpackages.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+python /usr/src/freebsd-pkg-builder/pkg-builder.py /usr/src/nopoll /usr/src/nopoll/freebsd/freebsd\:9\:x86\:64/ --skip-build --version=`cat /usr/src/nopoll/VERSION` --description="WebSocket OpenSource implementation" --outdir=/usr/src/freebsd-packages
+
