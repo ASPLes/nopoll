6765faa917701301f545e85b8fd2b51970cc5466
Francis Brosnan Blázquez <francis@aspl.es>
2015-12-15 17:57:13 +0100

    nopoll: * [fix] Updated free bsd build scripts to support more archs..

--- freebsd/buildpackages.sh
+++ freebsd/buildpackages.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-python /usr/src/freebsd-pkg-builder/pkg-builder.py /usr/src/nopoll /usr/src/nopoll/freebsd/9-x86-64/ --skip-build --version=`cat /usr/src/nopoll/VERSION` --description="WebSocket OpenSource implementation" --outdir=/usr/src/freebsd-packages
+python /usr/src/freebsd-pkg-builder/pkg-builder.py /usr/src/nopoll /usr/src/nopoll/freebsd/arch/ --skip-build --version=`cat /usr/src/nopoll/VERSION` --description="WebSocket OpenSource implementation" --outdir=/usr/src/freebsd-packages
 
