4ff260aa0d0eec16acf603f62b44e5b515e3b876
Francis Brosnan Blázquez <francis@aspl.es>
2016-11-16 20:35:45 +0100

    nopoll: * [fix] Fixed minor changes to build nopoll for freebsd..

--- freebsd/buildpackages.sh
+++ freebsd/buildpackages.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-python /usr/src/freebsd-pkg-builder/pkg-builder.py /usr/src/nopoll /usr/src/nopoll/freebsd/arch/ --skip-build --version=`cat /usr/src/nopoll/VERSION` --description="WebSocket OpenSource implementation" --outdir=/usr/src/freebsd-packages
+python /usr/src/freebsd-pkg-builder/pkg-builder.py /usr/src/nopoll/trunk /usr/src/nopoll/trunk/freebsd/arch/ --skip-build --version=`cat /usr/src/nopoll/trunk/VERSION` --description="WebSocket OpenSource implementation" --outdir=/usr/src/freebsd-packages
 
