41d06366b1f814a32feff58ef2d4906a25b3a37e
Francis Brosnan Blázquez <francis@aspl.es>
2017-09-19 13:36:52 +0200

    nopoll: * [fix] Updated nopoll_loop_wait to report -3 and -4 to indicate timeout   reached and to indicate I/O wait engine failure. This way, caller can catch   these codes and implement recovery and any other policy. Added documentation   to explain how to use this.
      Thanks for Tony Mountifield (reported the problem).

--- src/nopoll_loop.c
+++ src/nopoll_loop.c
@@ -136,9 +136,19 @@ nopoll_bool nopoll_loop_process (noPollCtx * ctx, noPollConn * conn, noPollPtr u
 }
 
 /** 
- * @internal Function used to init internal io wait mechanism...
+ * @internal Function used to init internal io wait mechanism
+ * associated to the provided context. If the io wait engine is
+ * already initialized, the function does nothing.
  *
  * @param ctx The noPoll context to be initialized if it wasn't
+ *
+ * In general this function is only used internally by noPoll to
+ * create io wait engine. In the case you are using \ref
+ * nopoll_loop_wait, and it returns -4 (io wait engine failed) then
+ * the I/O wait engine is released. In such condition, if you want to
+ * recover, you can use this function to restart io wait engine and
+ * call again nopoll_loop_wait.
+ *
  */
 void nopoll_loop_init (noPollCtx * ctx) 
 {
@@ -186,8 +196,34 @@ void nopoll_loop_stop (noPollCtx * ctx)
  * until a call to \ref nopoll_loop_stop is done in the case timeout
  * passed is 0.
  *
- * @return The function returns 0 when finished or -2 in the case ctx
- * is NULL or timeout is negative.
+ * @return The function returns 0 when finished without error or -2 in
+ * the case ctx is NULL or timeout is negative. Function returns -3 if
+ * timeout was reached. Function returns -4 in the case
+ * ctx->io_engine->wait failed to implement wait or it reported error.
+ *
+ *
+ * <b>Recovering from IO Wait failure (return code -4)</b>
+ *
+ * In the case I/O wait mechanism fails, this function will return
+ * -4. In can catch that error code and recover (keep on waiting), log
+ * the error or implement some other policy.
+ *
+ * Here is an example:
+ *
+ * \code
+ * while (nopoll_true) {
+ *     // wait for ever
+ *     int error_code = nopoll_loop_wait (ctx, 0);
+ *
+ *     if (error_code == -4) {
+ *          printf ("Log here you had an error cause by the io waiting mechanism, errno=%d\n", errno);
+ *          // recover by just calling io wait engine
+ *          // try to limit recoveries to avoid infinite loop
+ *          continue;
+ *     }
+ * \endcode
+ *
+ * <b>
  */
 int nopoll_loop_wait (noPollCtx * ctx, long timeout)
 {
@@ -196,6 +232,7 @@ int nopoll_loop_wait (noPollCtx * ctx, long timeout)
 	struct timeval diff;
 	long           ellapsed;
 	int            wait_status;
+	int            result = 0;
 
 	nopoll_return_val_if_fail (ctx, ctx, -2);
 	nopoll_return_val_if_fail (ctx, timeout >= 0, -2);
@@ -236,6 +273,7 @@ int nopoll_loop_wait (noPollCtx * ctx, long timeout)
 		/* nopoll_log (ctx, NOPOLL_LEVEL_DEBUG, "Waiting finished with result %d", wait_status);  */
 		if (wait_status == -1) {
 			nopoll_log (ctx, NOPOLL_LEVEL_CRITICAL, "Received error from wait operation, error code was: %d", errno);
+			result = -4; /* io wait failure */
 			break;
 		} /* end if */
 
@@ -255,8 +293,10 @@ int nopoll_loop_wait (noPollCtx * ctx, long timeout)
 #endif
 			nopoll_timeval_substract (&stop, &start, &diff);
 			ellapsed = (diff.tv_sec * 1000000) + diff.tv_usec;
-			if (ellapsed > timeout)
+			if (ellapsed > timeout) {
+				result = -3; /* timeout reached */
 				break;
+			}
 		} /* end if */
 	} /* end while */
 
@@ -264,7 +304,8 @@ int nopoll_loop_wait (noPollCtx * ctx, long timeout)
 	nopoll_io_release_engine (ctx->io_engine);
 	ctx->io_engine = NULL;
 
-	return 0;
+	/* return result so far */
+	return result;
 }
 
 /* @} */
