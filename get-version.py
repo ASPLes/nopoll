5b17a7472a866747dbc98ae83849527de1e60c89
Francis Brosnan Blázquez <francis@aspl.es>
2019-09-26 17:27:34 +0200

    nopoll: * [fix] Updated get-version.py to support loading versioning   from LATEST-VERSION.

--- get-version.py
+++ get-version.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python
 
-from core_admin_common import command
+from core_admin_common import command, support
 import sys
 
-(status, info) = command.run ("LANG=C svn update . | grep 'At revision'")
+(osname, oslongname, osversion) = support.get_os ()
+release_name = osversion.split (" ")[1]
+no_github_com_access = ["lenny", "squeeze"]
+
+if release_name in no_github_com_access:
+    command.run ("cp -f LATEST-VERSION VERSION")
+    sys.exit (0)
+# end if
+
+(status, info) = command.run ("LANG=C svn update . | grep revision")
 if status:
     print "ERROR: unable to get subversion version: %s" % info
     sys.exit (-1)
@@ -17,10 +26,10 @@ version = "%s.b%s" % (version, revision)
 print "INFO: Updated vesion to: %s" % version
 
 open ("VERSION", "w").write ("%s\n" % version)
+open ("LATEST-VERSION", "w").write ("%s\n" % version)
 
 # also update Changelog
 command.run ("svn log > Changelog")
-
 
 
 
