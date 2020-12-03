732ecbf002293511ae57bbaf147363d57dc7078d
Francis Brosnan Blázquez <francis@aspl.es>
2020-12-03 16:18:55 +0100

    nopoll: * [fix] Copyright updates

--- src/nopoll_ctx.h
+++ src/nopoll_ctx.h
@@ -1,6 +1,6 @@
 /*
  *  LibNoPoll: A websocket library
- *  Copyright (C) 2017 Advanced Software Production Line, S.L.
+ *  Copyright (C) 2020 Advanced Software Production Line, S.L.
  *
  *  This program is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public License
