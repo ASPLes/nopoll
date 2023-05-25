f24356023822542d81a3d7449ff3a6968ff87493
Francis Brosnan Blázquez <francis@aspl.es>
2023-05-25 16:48:36 +0200

    nopoll: * [fix] Updated certificates for regression tests

--- test/gen-certificates-test-21.sh
+++ test/gen-certificates-test-21.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+set -x
+# stop on failure
+set -e 
+
 days=1024
 
 # generate root key for CA
@@ -13,7 +17,7 @@ openssl genrsa -out root.key 2048
 
 # self sign CA certificate
 echo "INFO: generating root certificate"
-echo "ES\nMadrid\nMadrid\nASPL\nTI Support\nRoot\nsoporte@aspl.es\n" | openssl req -x509 -new -nodes -key root.key -days $days -out root.pem
+echo -e "ES\nMadrid\nMadrid\nASPL\nTI Support\nRoot\nsoporte@aspl.es\n" | openssl req -x509 -new -nodes -key root.key -days $days -out root.pem
 
 # join this two certificates into a single file
 cat root.pem root.key > root.pem2
@@ -24,7 +28,7 @@ rm -f root.key
 echo "INFO: generate server key"
 openssl genrsa -out server.key 2048
 
-echo "ES\nMadrid\nMadrid\nASPL\nTI Support\nserver.nopoll.aspl.es\nsoporte@aspl.es\n\n\n" | openssl req -new -key server.key -out server.csr
+echo -e "ES\nMadrid\nMadrid\nASPL\nTI Support\nserver.nopoll.aspl.es\nsoporte@aspl.es\n\n\n" | openssl req -new -key server.key -out server.csr
 # openssl req -new -key server.key -out server.csr
 
 # Sign certificate
@@ -41,7 +45,7 @@ rm -f server.csr
 echo "INFO: generate client key"
 openssl genrsa -out client.key 2048
 
-echo "ES\nMadrid\nMadrid\nASPL\nTI Support\nclient.nopoll.aspl.es\nsoporte@aspl.es\n\n\n" | openssl req -new -key client.key -out client.csr
+echo -e "ES\nMadrid\nMadrid\nASPL\nTI Support\nclient.nopoll.aspl.es\nsoporte@aspl.es\n\n\n" | openssl req -new -key client.key -out client.csr
 # openssl req -new -key server.key -out server.csr
 
 # Sign certificate
@@ -49,7 +53,7 @@ openssl x509 -req -in client.csr -CA root.pem -CAkey root.pem -CAcreateserial -o
 
 echo "INFO: copy file"
 cat client.pem2 client.key > client.pem
-rm client.pem2
+rm -f client.pem2
 rm -f client.key
 rm -f client.csr
 
