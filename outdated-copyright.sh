fa8b8c90a69fed63b1f1e4f7e9e95672f34c054f
Francis Brosnan Blázquez <francis.brosnan@aspl.es>
2025-01-02 16:15:41 +0000

    nopoll: * [fix] Updated copyright

--- outdated-copyright.sh
+++ outdated-copyright.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+rm -rf doc/html
+rm -rf debian/tmp
+rm -rf debian/libnopoll0-dev
+
 # find all files that have copy right declaration associated to Aspl that don't have 
 # the following declaration year
-current_year="2024"
+current_year="2025"
 LANG=C rgrep "Copyright" doc src web debian-files test rpm 2>&1 | grep "Advanced" | grep -v "Permission denied" | grep -v '~:' | grep -v '/\.svn/' | grep -v "${current_year}"
